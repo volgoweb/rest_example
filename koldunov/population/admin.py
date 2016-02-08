c3f4abdb191df21429283e7e0eb2bb340d91d242
volgoweb <volgoweb@bk.ru>
2016-02-08 20:58:03 +0300

    Add population to admin page.

--- koldunov/population/admin.py
+++ koldunov/population/admin.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Population
+
+
+class PopulationAdmin(admin.ModelAdmin):
+    list_display = ('city', 'country', 'population')
+    search_fields = ('country', 'city')
+    list_filter = ('country',)
+
+
+admin.site.register(Population, PopulationAdmin)
