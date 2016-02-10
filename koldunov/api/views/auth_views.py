4e79d0c1fd1ccf35cb15af4a094c125406fd932a
volgoweb <volgoweb@bk.ru>
2016-02-10 12:14:13 +0300

    Added logout view

--- koldunov/api/views/auth_views.py
+++ koldunov/api/views/auth_views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
+import json
 from django.middleware.csrf import _get_new_csrf_key
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.http.response import HttpResponse
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -16,10 +18,16 @@ class Login(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
-        login(request, user)
+        auth_login(request, user)
         response = Response({'token': token.key})
         csrf_token = _get_new_csrf_key()
         response.set_cookie('csrftoken', csrf_token)
         return response
 
 
+def logout(request):
+    auth_logout(request)
+    response = HttpResponse(json.dumps({'status': 'Logged out'}))
+    response.delete_cookie('csrftoken')
+    response.delete_cookie('sessionid')
+    return response
--- koldunov/urls.py
+++ koldunov/urls.py
@@ -16,13 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
-from koldunov.api.views.auth_views import Login
+from koldunov.api.views.auth_views import Login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^app/auth/login/', Login.as_view(), name="login"),
-    url(r'^app/auth/login/', auth_views.logout, name="logout"),
+    url(r'^app/auth/logout/', logout, name="logout"),
     url(r'^app/api/', include('koldunov.api.urls')),
 ]
