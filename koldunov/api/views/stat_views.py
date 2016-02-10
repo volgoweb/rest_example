07db63ab046df2107814ea4b375f373bee26ea27
volgoweb <volgoweb@bk.ru>
2016-02-10 08:35:34 +0300

    Disable any operation except read for stat api.

--- koldunov/api/views/stat_views.py
+++ koldunov/api/views/stat_views.py
@@ -3,10 +3,8 @@ from rest_framework import viewsets
 
 from koldunov.population.models import Population
 from koldunov.api.serializers.stat_serializers import StatSerializer
-from koldunov.api.permissions import ReadOnly
 
 
-class StatViewSet(viewsets.ModelViewSet):
+class StatViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Population.objects.all()
     serializer_class = StatSerializer
-    permission_classes = [ReadOnly]
